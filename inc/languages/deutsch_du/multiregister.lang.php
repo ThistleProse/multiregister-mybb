24cbf46ac85d0644320cf52e356155c648b4627d
ThistleProse <thistleprose@gmail.com>
2026-02-18 19:52:37 +1100

    Delete inc/languages/deutsch_du directory

--- inc/languages/deutsch_du/multiregister.lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$l['usercp_nav_multiregister'] = "Mehrcharakter erstellen";
-$l['send_character'] = "Charakter absenden";
-$l['masteraccount'] = "Dieser Charakter wird verbunden mit:";
-
-?>
